b97f4661a4fe7a939009191c84996a54bd337f01
Xiaohan Wu <wuxiaohan.a@gmail.com>
2024-08-16 23:08:29 +0100

    adjusted vector color for right and wrong answers

---
+++ Assets/Scripts/Illustrator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Illustrator : MonoBehaviour
+{
+    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private MeshRenderer meshRenderer;
+    [SerializeField] private Material rightAnswerMaterial;
+    [SerializeField] private Material wrongAnswerMaterial;
+
+    public void ApplyRightAnswerMat()
+    {
+        meshRenderer.material = rightAnswerMaterial;
+        lineRenderer.material = rightAnswerMaterial;
+    }
+
+    public void ApplyWrongAnswerMat()
+    {
+        meshRenderer.material = wrongAnswerMaterial;
+        lineRenderer.material = wrongAnswerMaterial;
+    }
+
+    public LineRenderer GetLineRenderer()
+    {
+        return lineRenderer;
+    }
+}
